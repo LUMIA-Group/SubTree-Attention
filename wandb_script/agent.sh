97775349939c0047d1e9c0b0c8e1261df9745fdb
siyuanhuang <siyuan_huang_SJTU@outlook.com>
2023-05-22 04:29:30 +0200

    minor fix

--- wandb_script/agent.sh
+++ wandb_script/agent.sh
@@ -1,1 +1,1 @@
-python wandb_script/agents.py --entity=syhuang_sjtu --project=NoGlobal --sweep_id=rstoyyew --gpu_allocate=0:1 --wandb_base=remote --mode=parallel --save_model=False
+python wandb_script/agents.py --entity=syhuang_sjtu --project=NoGlobal --sweep_id=dlfffoim --gpu_allocate=0:1 --wandb_base=remote --mode=parallel --save_model=False
